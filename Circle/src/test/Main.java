5a07cd9efa5b6d85f7c0574f3d49980c00db0014
Elmery <mohameddelmery@gmail.com>
2025-07-30 02:22:13 +0200

    version 2 of figure 9.3

--- Circle/src/test/Main.java
+++ Circle/src/test/Main.java
@@ -6,6 +6,9 @@ public class Main
     public static void main(String[] args)
     {
         Circle1 c1 = new Circle1();
-        
+
+        c1.setRadius(5);
+
+        System.out.println(c1.getArea());
     }
 }
